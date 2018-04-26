e29f13d5da0dad9eb3d2a8c261fbb5e6c0742da8
HanYoungsoo <gglife333@gmail.com>
2018-04-27 02:19:17 +0900

    rm example folder

--- examples/src/TimeManager.js
+++
@@ -1,6 +0,0 @@
-export default class TimeManager {
-  static randomeTimeCheck(savedTime, now, invisibleTime) {
-    const passdTime = Math.floor((now - savedTime) / (6 * Math.pow(10, 4)))
-    return now - savedTime > invisibleTime * (6 * Math.pow(10, 4)) // minute
-  }
-}
