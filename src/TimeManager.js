9b31a652ec2f42f287e231802a4fe9e6c0e42dc0
youngsoohan <amazingmobdev@gmail.com>
2019-04-07 23:29:57 +0900

    remove unused line

--- src/TimeManager.js
+++ src/TimeManager.js
@@ -1,6 +1,7 @@
-export default class TimeManager {
-  static randomeTimeCheck(savedTime, now, invisibleTime) {
-    const passdTime = Math.floor((now - savedTime) / (6 * Math.pow(10, 4)))
+const TimeManager = {
+  randomeTimeCheck(savedTime, now, invisibleTime) {
     return now - savedTime > invisibleTime * (6 * Math.pow(10, 4)) // minute
-  }
+  },
 }
+
+export default TimeManager
